bd8385b92890361b03dfabf85e49fa8d5613efd7
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-10 16:47:12 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -10,7 +10,10 @@ import asyncio
 from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, UniqueConstraint, func
 
 
-from sample_config import Config
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
 
 
 def start() -> scoped_session:
@@ -25,39 +28,39 @@ SESSION = start()
 
 INSERTION_LOCK = threading.RLock()
 
-class custom_caption(BASE):
-    __tablename__ = "caption"
+class Thumbnail(BASE):
+    __tablename__ = "thumbnail"
     id = Column(Integer, primary_key=True)
-    caption = Column(String)
+    msg_id = Column(Integer)
     
-    def __init__(self, id, caption):
+    def __init__(self, id, msg_id):
         self.id = id
-        self.caption = caption
+        self.msg_id = msg_id
 
-custom_caption.__table__.create(checkfirst=True)
+Thumbnail.__table__.create(checkfirst=True)
 
-async def update_cap(id, caption):
+async def df_thumb(id, msg_id):
     with INSERTION_LOCK:
-        cap = SESSION.query(custom_caption).get(id)
-        if not cap:
-            cap = custom_caption(id, caption)
-            SESSION.add(cap)
+        msg = SESSION.query(Thumbnail).get(id)
+        if not msg:
+            msg = Thumbnail(id, msg_id)
+            SESSION.add(msg)
             SESSION.flush()
         else:
-            SESSION.delete(cap)
-            cap = custom_caption(id, caption)
-            SESSION.add(cap)
+            SESSION.delete(msg)
+            file = Thumbnail(id, msg_id)
+            SESSION.add(file)
         SESSION.commit()
 
-async def del_caption(id):
+async def del_thumb(id):
     with INSERTION_LOCK:
-        msg = SESSION.query(custom_caption).get(id)
+        msg = SESSION.query(Thumbnail).get(id)
         SESSION.delete(msg)
         SESSION.commit()
 
-async def get_caption(id):
+async def thumb(id):
     try:
-        caption = SESSION.query(custom_caption).get(id)
-        return caption
+        t = SESSION.query(Thumbnail).get(id)
+        return t
     finally:
-        SESSION.stop()
+        SESSION.close()
